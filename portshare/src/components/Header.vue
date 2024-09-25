d4229dcdd8edb646753eee9ad6c2a49f12d7812d
dw33d <170299648+dw33d@users.noreply.github.com>
2024-09-25 21:51:48 +0200

    Init commit

---
+++ portshare/src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+    <header >
+        <h1 :class="$style.cream">Welcome To PortShare.</h1>
+        <h2 :class="$style.cream">Where development meets portfolio based improvement</h2>
+    </header>
+</template>
+
+<style module>
+    .cream {
+        color: rgb(	255, 253,208);
+    }
+</style>
