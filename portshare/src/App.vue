f4896aff4d31ed963c2783f2079585bedb4a57d4
dw33d <170299648+dw33d@users.noreply.github.com>
2024-09-26 22:20:53 +0200

    Added Landing component, made UI changes

--- portshare/src/App.vue
+++ portshare/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import Header from './components/Header.vue'
+  import Landing from './components/Landing.vue';
 </script>
 
 <template>
   <Header />
+  <Landing />
 </template>
--- portshare/src/components/Header.vue
+++ portshare/src/components/Header.vue
@@ -1,12 +1,16 @@
 <template>
     <header >
-        <h1 :class="$style.cream">Welcome To PortShare.</h1>
-        <h2 :class="$style.cream">Where development meets portfolio based improvement</h2>
+        <h1 class="cream">PortShare.</h1>
+        <h2 class="cream">Where development meets portfolio based improvement</h2>
     </header>
 </template>
 
-<style module>
+<style scoped>
     .cream {
         color: rgb(	255, 253,208);
+        margin-top: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
 </style>
